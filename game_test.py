86e2168842e7a6e9605b13a2b95822e6421d1b06
kTurek95 <k.turek1995@gmail.com>
2023-08-01 22:40:14 +0100

    test for game class

---
+++ game_test.py
@@ -0,0 +1,15 @@
+from game import Game
+from deck import Deck
+from player import Player
+
+
+def test_croupier_plays():
+    pass
+
+
+def test_user_plays():
+    pass
+
+
+def test_play():
+    pass
